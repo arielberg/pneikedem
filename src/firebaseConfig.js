cd471eacbd16044c008e01fca64f488c3943c56d
Ariel Berg <arielberg@gmail.com>
2025-06-16 16:51:38 +0300

    working poc

---
+++ src/firebaseConfig.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAoOzH_i6sQoQq59NEXZzi-JcWplENTGfw",
+    authDomain: "famelies.firebaseapp.com",
+    projectId: "famelies",
+    storageBucket: "famelies.firebasestorage.app",
+    messagingSenderId: "876298055884",
+    appId: "1:876298055884:web:022e5a07599ceafe829269",
+    measurementId: "G-8CM4RFVH1W"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+export default db;
