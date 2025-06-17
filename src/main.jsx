d3cfb01a815e5dcceb6119f1c0c881a53e504bf8
Ariel Berg <arielberg@gmail.com>
2025-06-17 16:17:16 +0300

    install button

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import ResidentApp from './app.jsx';
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register('src/worker.js')
+        .register('worker.js')
         .then((reg) => console.log('SW registered:', reg))
         .catch((err) => console.error('SW registration failed:', err));
     });
